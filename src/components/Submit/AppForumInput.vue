f2378aff2e8459e0e98087c13edfc7d5f37991c3
Severn Lortie <severn@Severns-MacBook-Pro.local>
2020-05-13 01:10:09 -0400

    fix button cols

--- src/components/Submit/AppForumInput.vue
+++ src/components/Submit/AppForumInput.vue
@@ -18,7 +18,7 @@
       ></v-textarea>
       <div class="grey--text text--darken-1">Note: your story must be longer than 100 characters</div>
     </v-col>
-    <v-col cols="11" class="text-right">
+    <v-col cols="12" class="text-right">
       <v-btn class="text-capitalize" color="accent" @click="$emit('submitted', forum)">Submit</v-btn>
     </v-col>
   </v-row>
