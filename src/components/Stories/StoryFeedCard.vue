488004a6c876a46ff2ad3870af577fd7ddd2fe76
Severn Lortie <severn@Severns-MacBook-Pro.local>
2020-05-13 15:48:33 -0400

    add text trunctation

--- src/components/Stories/StoryFeedCard.vue
+++ src/components/Stories/StoryFeedCard.vue
@@ -2,7 +2,7 @@
   <v-card flat v-if="story">
     <v-card-title>{{story.title}}</v-card-title>
     <v-card-subtitle>{{story.author}}</v-card-subtitle>
-    <v-card-text>{{story.text.substring(0, 250)}}</v-card-text>
+    <v-card-text>{{story.text.substring(0, 257).trim() + "..."}}</v-card-text>
     <v-card-actions>
       <app-btn text :to="`/story/${story.id}`">Read More</app-btn>
     </v-card-actions>
