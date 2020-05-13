cb301af1a42076101a83c05c8f8bd9f7900af644
Severn Lortie <severn@Severns-MacBook-Pro.local>
2020-05-13 16:20:58 -0400

    lazy load imagesi n gallery

--- src/components/Photos/PhotoGalleryCard.vue
+++ src/components/Photos/PhotoGalleryCard.vue
@@ -1,39 +1,39 @@
 <template>
-<v-hover
-    v-slot:default="{ hover }"
->
-    <v-card
-        flat
-        tile
-        @click.stop="$emit('clicked', meta.id)"
-        :ripple="false"
-    >
-        <v-img
-            :src="meta.photo.url"
-            aspect-ratio="1"
-        >
-            <template v-slot:placeholder>
-                <image-progress/>
-            </template>
-            <card-subtitle
-                :display="hover || $vuetify.breakpoint.xs"
-                v-if="meta.photo.subtitle"
-            >
-                {{meta.photo.subtitle}}
-            </card-subtitle>
+  <v-lazy
+    v-model="isActive"
+    :options="{
+        threshold: .5
+    }"
+    min-height="200"
+    transition="fade-transition"
+  >
+    <v-hover v-slot:default="{ hover }">
+      <v-card flat tile @click.stop="$emit('clicked', meta.id)" :ripple="false">
+        <v-img :src="meta.photo.url" aspect-ratio="1">
+          <template v-slot:placeholder>
+            <image-progress />
+          </template>
+          <card-subtitle
+            :display="hover || $vuetify.breakpoint.xs"
+            v-if="meta.photo.subtitle"
+          >{{meta.photo.subtitle}}</card-subtitle>
         </v-img>
-    </v-card>
-</v-hover>
+      </v-card>
+    </v-hover>
+  </v-lazy>
 </template>
 
 <script>
 export default {
-    props: {
-        meta: Object
-    },
-    components: {
-        cardSubtitle: () => import('./PhotoGalleryCardSubtitle'),
-        imageProgress: () => import("../core/AppImageProgress")
-    }
-}
+  data: () => ({
+    isActive: false
+  }),
+  props: {
+    meta: Object
+  },
+  components: {
+    cardSubtitle: () => import("./PhotoGalleryCardSubtitle"),
+    imageProgress: () => import("../core/AppImageProgress")
+  }
+};
 </script>
