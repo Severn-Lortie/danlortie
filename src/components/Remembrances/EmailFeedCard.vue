875891abb4210c42a41ad9066dbababb4bb91d1c
Severn Lortie <severn@Severns-MacBook-Pro.local>
2020-05-18 21:54:32 -0400

    added email feed card

---
+++ src/components/Remembrances/EmailFeedCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card>
+    <v-sheet color="#272727" class="pl-4 pt-2" tile height="40">Email</v-sheet>
+    <v-card-subtitle>
+      <div class="mb-1 text-capitalize">From: {{ email.author }}</div>
+      <v-divider></v-divider>
+      <div class="mb-1 mt-1 text-capitalize">Subject: {{ email.subject }}</div>
+      <v-divider></v-divider>
+    </v-card-subtitle>
+    <v-card-text class="white--text">
+      <p v-line-clamp:30="2" class="whitespace">{{ email.text.trim() }}</p>
+    </v-card-text>
+    <v-card-actions>
+      <app-btn text :to="`view/remembrance/${email.id}`">Read More</app-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    email: Object
+  },
+  components: {
+    appBtn: () => import("../core/AppButton")
+  }
+};
+</script>
+
+<style scoped>
+.whitespace {
+  word-break: keep-all !important;
+}
+</style>
