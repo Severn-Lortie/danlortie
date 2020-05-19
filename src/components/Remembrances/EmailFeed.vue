369426bba5e1c1ee6df4dcddda44e9154d7aace6
Severn Lortie <severn@Severns-MacBook-Pro.local>
2020-05-18 21:54:24 -0400

    added email feed

---
+++ src/components/Remembrances/EmailFeed.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fluid class="fill-height">
+    <!-- margin row -->
+    <v-row align="start" justify="center">
+      <v-col cols="12" md="10" lg="9" class="ml-0 mr-0">
+        <!-- feed -->
+        <v-row align="start" justify="start">
+          <v-col v-for="email in emails" :key="email.id" cols="12" md="6">
+            <feed-card
+              :email="email"
+            />
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  components: {
+    FeedCard: () => import("./EmailFeedCard")
+  },
+  computed: {
+      emails() {
+          return this.$store.state.remembrance.emails;
+      }
+  }
+};
+</script>
