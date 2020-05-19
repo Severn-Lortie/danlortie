3356c5ceecf0b1c33326aed136818dcd8a3d94f4
Severn Lortie <severn@Severns-MacBook-Pro.local>
2020-05-18 21:52:42 -0400

    consolodate "viewers" into one viewer body comp.

---
+++ src/components/ViewerBody.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="body-1 whitespace">
+      <!-- main text slot -->
+      <vue-markdown class="whitespace" :source="source" />
+  </div>
+</template>
+
+<script>
+export default {
+    components: {
+        VueMarkdown: () => import('vue-markdown')
+    },
+    props: {
+        source: String
+    }
+}
+</script>
+
+<style scoped>
+.whitespace {
+    white-space: pre-line;
+    word-wrap: break-word;
+}
+</style>
